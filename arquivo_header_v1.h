01f80852aab3133fd12f39330467b460ec11e02d
CauanDev <109750240+CauanDev@users.noreply.github.com>
2023-04-23 02:15:10 -0400

    Update arquivo_header_v1.h

--- arquivo_header_v1.h
+++ arquivo_header_v1.h
@@ -1,29 +1,56 @@
 #include "console_v1.5.4.h"
 #include "conio_v3.2.4.h"
+#include "graphics_v1.1.h"
+#include <stdlib.h>
+#include <time.h>
+#include <Windows.h>
 
+typedef struct
+{
+    int top;
+    int right;
+    int left;
+    int bottom;
+}AREA;
 
-typedef struct{
+typedef struct
+{
+    int ladoDireito,ladoEsquerdo,ladoSuperior,ladoInferior;
+}LadoAcrescentar;
+
+typedef struct
+{
     int largura;
     int altura;    
     int direcao;
     int posX_Atual;
     int posY_Atual;
-    int top,right,left,bottom;
+    AREA area;
+    LadoAcrescentar lados;
+    int VELOCIDADE;
+    COLORS cor;
+    
 }QUADRADO;
 
-typedef struct{
+typedef struct
+{
     COORD dimensao;
     
 
 }TELA;
 
+
+
 void Quadrados(  QUADRADO * ,  COORD );
 void Atualiza_Centro( TELA * );
 void AtribuirTamanhoQuadrados(QUADRADO *,int ,int );
 void Start();
-void AjustarPosicaoQuadrado(int ,int );
-void ComandoDirecao(QUADRADO *, QUADRADO *);
-void SwitchDirecao(QUADRADO *, QUADRADO* );
-void ImprimirDirecao();
-void VerificarEvento(EVENTO );
+void AjustarPosicaoQuadrado(QUADRADO *);
+void ComandoDirecao(QUADRADO *, QUADRADO *, TELA *);
+void SwitchDirecao(QUADRADO *, QUADRADO* , TELA *);
+void AjustarCorQuadrado(QUADRADO *);
+void SwitchTeclas(DWORD , QUADRADO *,QUADRADO *, TELA * );
+void EscreverTela(QUADRADO*, QUADRADO*);
+void SwitchCor(COLORS , char[]);
+
     
